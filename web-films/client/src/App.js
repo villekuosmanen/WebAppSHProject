05483a545780f9cd7604f9555fa5ca788a0c6008
Ville Kuosmanen <vibe96@gmail.com>
2019-10-14 12:39:13 +0100

    Init project, connect client and server

---
+++ web-films/client/src/App.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+
+    state = {
+        response: '',
+        post: '',
+        responseToPost: '',
+    };
+
+    componentDidMount() {
+        this.callApi()
+            .then(res => this.setState({ response: res.express }))
+            .catch(err => console.log(err));
+    }
+
+    callApi = async () => {
+        const response = await fetch('/api/recommendations');
+        const body = await response.json();
+        if (response.status !== 200) throw Error(body.message);
+
+        return body;
+    };
+
+    render() {
+        return (
+            <div className="App">
+                <p>{this.state.response}</p>
+            </div>
+        );
+    }
+}
+
+export default App;
